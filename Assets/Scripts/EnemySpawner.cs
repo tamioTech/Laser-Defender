ff74b86ab05258b48ca50a4a810d04cc07f35d88
tamioTech <62820237+tamioTech@users.noreply.github.com>
2021-02-21 01:12:54 -0800

    add health and damage

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,12 +7,17 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] List<WaveConfig> waveConfigs;
     [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllWaves());
+        do
+        {
+            //var currentWave = waveConfigs[startingWave];
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
     }
 
     private IEnumerator SpawnAllWaves()
