f4473c779080de9cb7c7be47c05ce116a09ac301
tamioTech <62820237+tamioTech@users.noreply.github.com>
2021-02-27 16:24:23 -0800

    add audio assets

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BackgroundScroller : MonoBehaviour
 {
 
-    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    [SerializeField] float backgroundScrollSpeed = 0.2f;
     Material myMaterial;
     Vector2 offset;
 
